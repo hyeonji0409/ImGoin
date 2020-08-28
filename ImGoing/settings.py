d37e2a8c8e98c3682e88a5e18844a31487f99f1f
HTEONJI <guswl8507@naver.com>
2020-08-28 16:04:37 +0900

    second

--- ImGoing/settings.py
+++ ImGoing/settings.py
@@ -152,6 +152,6 @@ SITE_ID = 1
 # 로그인 후 호출 url
 LOGIN_REDIRECT_URL = '/'
 
-#import dj_database_url
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ImGoing.wsgi --log-file -
